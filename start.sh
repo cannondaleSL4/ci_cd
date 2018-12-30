38565b2078ae5a769ac461b954356a42e24b84f7
Dmitriy <workday2006@yandex.ru>
2018-12-31 01:07:15 +0300

    add postgres docker with volume running command

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# //docker postgres// https://hackernoon.com/dont-install-postgres-docker-pull-postgres-bee20e200198
+
 gateway="gateway"
 identity="identity"
 front="front"
@@ -10,7 +12,7 @@ savefiles="savefiles"
 postgres="postgres_10_vagrant"
 postgres_port="5433"
 export postgres_user="postgres"
-export postgres_password="postgres"
+export postgres_password="docker"
 export database_name="identity_management"
 STARS=$'\n****************************************************************************************\n'
 
@@ -51,44 +53,13 @@ mongo_path=""$HOME"/docker/volumes/mongo_db"
 mkdir -p "$mongo_path"
 
 echo "$STARS" "Run docker postgres database" "$STARS"
-docker run --rm   --name pg-docker -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v "$postgres_path":/var/lib/postgresql/data  postgres
+docker run --rm --name pg-docker -e POSTGRES_PASSWORD="$postgres_password" -d -p 5432:5432 -v "$postgres_path":/var/lib/postgresql/data  postgres
 
-#if [ ! -d "$postgres" ]; then
-#	cd ./"$postgres"/Vagrant
-#	git clone git@github.com:cannondaleSL4/postgres_10_vagrant.git
-#	sudo apt-get install vagrant -y
-#	echo "Vagrant app was installed"
-#	sudo apt-get install virtualbox -y
-#	echo "Virtual-box app was installed"
-#	VBoxManage modifyvm "Vagrant_default_1545511812985_93148" --natpf1 "guestssh,tcp,,2222,,22"
-#	echo "Vagrant was configured"
-#	vagrant up
-#	vagrant halt
-#	vagrant up
-#	echo "Vagrant has been started"
-#	vagrant ssh-config > vagrant-ssh
-#else
-#	cd ./"$postgres"/Vagrant
-#	vagrant_potgres=$(sudo netstat -anltp | grep :22 | grep LISTEN)
-#	if [ -z "$vagrant_potgres" ]; then
-#		vagrant up
-#	fi
-#fi
-#
-#connection_command="PGPASSWORD="$postgres_password" psql -U "$postgres_user" -h localhost"
-#create_command="create database "$database_name""
-#grant_command="grant all privileges on database "$database_name" to "$postgres_user""
-#
-#ssh -F vagrant-ssh default scp ../../db.sh vagrant@localhost:/home/vagrant/db.sh
+echo "$STARS" "Install postgres client" "$STARS"
+sudo apt install postgresql-client
 
-# ssh -p 2222 -i /home/dima/java_projects/myproject/postgres_10_vagrant/Vagrant/.vagrant/machines/default/virtualbox/private_key vagrant@localhost "psql -U postgres -h localhost"
 
-# PGPASSWORD="$postgres_password" psql -U "$postgres_user" -h localhost
-# create database "$database_name";
-# grant all privileges on database "$database_name" to "$postgres_user"
-
-#cd "$main_dir"
-
+PGPASSWORD="$postgres_password" psql -U "$postgres_user" -h localhost
 
 
 
