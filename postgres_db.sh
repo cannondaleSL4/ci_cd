bd1048fd6534ff6e5db53eaaedeee6f0f341b366
Dmitriy <workday2006@yandex.ru>
2019-03-25 13:00:21 +0300

    add postgres docker with volume running command

--- postgres_db.sh
+++ postgres_db.sh
@@ -6,6 +6,10 @@ postgres_user="postgres"
 postgres_password="docker"
 database_name="identity_management"
 postgres_path=""$HOME"/docker/volumes/postgres"
+pg_docker_name="pg-docker"
+#command_psql="PGPASSWORD="$postgres_password" psql -U "$postgres_user" -h localhost"
+command_psql="psql -h localhost -U postgres -d postgres"
+
 
 
 echo "$STARS" "Try to get docker images with postgres" "$STARS"
@@ -20,8 +24,12 @@ echo "$STARS" "Install postgres client" "$STARS"
 sudo apt install postgresql-client
 
 echo "$STARS" "Run docker postgres database" "$STARS"
-command_psql="PGPASSWORD="$postgres_password" psql -U "$postgres_user" -h localhost"
-docker run --rm --name pg-docker -e POSTGRES_PASSWORD="$postgres_password" -d -p 5432:5432 -v "$postgres_path":/var/lib/postgresql/data  postgres -а
+docker rm "$pg_docker_name" -f
+docker run -e POSTGRES_PASSWORD="$postgres_password" -d -p 5432:5432 -v "$postgres_path":/var/lib/postgresql/data postgres --rm --name "$pg_docker_name"
+docker cp "$MAIN_DIR"/create_db.sql pg-docker:/file.sql
+docker exec -it pg-docker "$command_psql"
+
+#-f /file.sql
 
 #"$main_dir"/create_db.sql
 
--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -x
+
 # //docker postgres// https://hackernoon.com/dont-install-postgres-docker-pull-postgres-bee20e200198
 
 gateway="gateway"
@@ -9,12 +11,9 @@ eureka="eureka"
 persist="persist"
 request="request"
 savefiles="savefiles"
-#postgres="postgres_10_vagrant"
-#postgres_port="5433"
-#export postgres_user="postgres"
-#export postgres_password="docker"
-#export database_name="identity_management"
 export STARS=$'\n****************************************************************************************\n'
+
+export MAIN_DIR=$(pwd)
 
 cd ../
 
@@ -25,17 +24,17 @@ if [[ ! -d "$eureka" ]]; then git clone git@github.com:cannondaleSL4/eureka.git 
 if [[ ! -d "$persist" ]]; then git clone git@github.com:cannondaleSL4/persist.git ; fi
 if [[ ! -d "$request" ]]; then git clone git@github.com:cannondaleSL4/request.git ; fi
 if [[ ! -d "$savefiles" ]]; then git clone git@github.com:cannondaleSL4/savefiles.git ; fi
+if [[ ! -d "$entity" ]]; then git clone git@github.com:cannondaleSL4/entity.git ; fi
+
 
 echo "$STARS" "All projects was cloned" "$STARS"
-
-main_dir=$(pwd)
 
 echo "install util docker"
 sudo apt install docker.io -y
 sudo usermod -a -G docker $USER
 sudo chmod 666 /var/run/docker.sock
 
-chmod +x "$main_dir"/postgres_db.sh && source "$main_dir"/postgres_db.sh
+chmod +x "$MAIN_DIR"/postgres_db.sh && source "$MAIN_DIR"/postgres_db.sh
 
 
 #echo "$STARS" "Try to get docker images with mongo db" "$STARS"
